372a792478636c31208180e9bdc075231e6958d8
Pierre Plagnes <pierre.plagnes@gmail.com>
2021-01-24 17:02:31 +0100

    dotnet samples

---
+++ src/end-of-cure-producer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Coding4Fun.EndOfCure;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Confluent.Kafka;
+
+namespace end_of_cure_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string bootstrapServers = "127.0.0.1:9092";
+            string schemaRegistryUrl = "127.0.0.1:8081";
+            string topicName = "end-of-cure";
+
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = bootstrapServers
+                
+            };
+
+            var schemaRegistryConfig = new SchemaRegistryConfig
+            {
+                Url = schemaRegistryUrl
+            };
+
+            var avroSerializerConfig = new AvroSerializerConfig
+            {
+                BufferBytes = 100
+            };
+
+            using (var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig))
+            using (var producer = new ProducerBuilder<string, EndOfCure>(producerConfig)
+                    .SetKeySerializer(new AvroSerializer<string>(schemaRegistry, avroSerializerConfig))
+                    .SetValueSerializer(new AvroSerializer<EndOfCure>(schemaRegistry, avroSerializerConfig))
+                    .Build())
+            {
+                while (true)
+                {
+                    var evt = new EndOfCure();
+                    evt.Date = DateTime.Now.ToLongDateString();
+                    evt.ItemCode = DateTime.Now.Ticks.ToString();
+                    evt.CureEquipmentId = "001";
+
+                    producer
+                            .ProduceAsync(topicName, new Message<string, EndOfCure> { Key = evt.CureEquipmentId, Value = evt })
+                            .ContinueWith(task =>
+
+                                {
+                                    if (!task.IsFaulted)
+                                        Console.WriteLine($"produced to: {task.Result.TopicPartitionOffset}");
+                                    Console.WriteLine($"error producing message: {task.Exception.InnerException}");
+                                });
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ src/enhanced-end-of-cure-consumer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Coding4Fun.EnhancedEndOfCure;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Confluent.Kafka;
+
+namespace enhanced_end_of_cure_consumer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string bootstrapServers = "127.0.0.1:9092";
+            string schemaRegistryUrl = "127.0.0.1:8081";
+            string topicName = "enhanced-end-of-cure";
+
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = "console-group"
+            };
+
+            var schemaRegistryConfig = new SchemaRegistryConfig
+            {
+                Url = schemaRegistryUrl
+            };
+
+            var avroSerializerConfig = new AvroSerializerConfig
+            {
+            };
+
+            using (var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig))
+            using (var consumer = new ConsumerBuilder<string, EnhancedEndOfCure>(consumerConfig)
+                    .SetKeyDeserializer(new AvroDeserializer<string>(schemaRegistry, avroSerializerConfig).AsSyncOverAsync())
+                    .SetValueDeserializer(new AvroDeserializer<EnhancedEndOfCure>(schemaRegistry, avroSerializerConfig).AsSyncOverAsync())
+                    .Build())
+            {
+                consumer.Subscribe(topicName);
+
+                try
+                {
+                    CancellationTokenSource cts = new CancellationTokenSource();
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumeResult = consumer.Consume(cts.Token);
+                            var v = (EnhancedEndOfCure)consumeResult.Message.Value;
+                            Console.WriteLine($"End Of cure : \n\r" +
+                                " - Date : " + v.Date +
+                                " - ItemCode : " + v.ItemCode +
+                                " - CureEquipmentId : " + v.CureEquipmentId +
+                                " - ShiftCode : " + v.ShiftCode);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Consume error: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
